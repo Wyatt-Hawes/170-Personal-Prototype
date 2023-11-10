d812beffc10aeb5c69ac97b8ed0ff3ce6cc08707
Wyatt H <jhawes@ucsc.edu>
2023-11-10 12:49:44 -0800

    Adding grass & more flytraps

--- docs/PersonalPrototype/main.js
+++ docs/PersonalPrototype/main.js
@@ -25,20 +25,28 @@ GGGGG
  GGG  
 `,
   `
- llll
-ll  ll
-l    l
-l    l
-ll  ll 
- llll 
-`,
-  `
      
   l  
  lll 
   l
    
       
+`,
+  `
+    ll
+lll ll
+   l
+   l  
+lll ll 
+    ll
+`,
+  `
+      
+    
+    ll 
+llll
+    ll 
+       
 `,
 ];
 
@@ -73,19 +81,23 @@ function update() {
   let scr = 0; //getScrVal();
   const SPEED_MULT = 0.3;
   //const angle = Math.sin(difficulty * 0.03 * (ticks / 3)) * 0.5 - 1.5707;
-  const angle = Math.sin(ticks / 60) * 0.5 - 1.5707;
+  const angle = calculateAngle(0);
 
   //Testing sprites visuals
   // const c1 = char("a", vec(50, 10)).isColliding.char;
   // const c2 = char("b", vec(60, 9)).isColliding.char;
-  // const c3 = char("c", vec(70, 10)).isColliding.char;
-  // const c4 = char("d", vec(80, 10)).isColliding.char;
+  //const c3 = char("c", vec(70, 10)).isColliding.char;
+  if (ticks % 20 < 10) {
+    const c4 = char("d", vec(80, 10)).isColliding.char;
+  } else {
+    const c5 = char("e", vec(80, 10.5)).isColliding.char;
+  }
 
   // Runs on first tick
   if (!ticks) {
     //flys = [vec(50, 100)]; // 'vec()' creates a 2d vector instance.
     nextPinDist = 10;
-    cord = { angle: 0, length: cordLength, pin: vec(50, 100) };
+    cord = { angle: 0, length: cordLength, pin: vec(50, 98) };
     //pins.push(vec(rnd(10, 90), -2 - nextPinDist));
     spawnFly();
   }
@@ -93,6 +105,9 @@ function update() {
   if (ticks % 60 == 0) {
     spawnFly();
   }
+
+  //Draw other plants
+  drawCosmetics();
 
   // Draw plant
   drawPlant(angle);
@@ -105,11 +120,57 @@ function update() {
   handleCordLength(SPEED_MULT);
 }
 
+function calculateAngle(offset) {
+  return Math.sin((ticks + offset) / 60) * 0.5 - 1.5707;
+}
+
 function handleCordLength(SPEED_MULT) {
   if (input.isPressed) {
     cord.length += difficulty * SPEED_MULT;
   } else {
     cord.length += (cordLength - cord.length) * 0.04 * SPEED_MULT;
+  }
+}
+
+function drawCosmetics() {
+  color("green");
+  rect(0, 99, 100, 1);
+
+  rect(5, 98, 10, 1);
+  rect(25, 98, 10, 1);
+  rect(42, 98, 15, 2);
+  rect(45, 97, 10, 2);
+
+  rect(79, 98, 8, 1);
+  rect(81, 97, 4, 1);
+
+  color("black");
+
+  drawFakePlant(vec(10, 100), 8, calculateAngle(-79), 73);
+
+  drawFakePlant(vec(5, 102), 5, calculateAngle(30), 60);
+  drawFakePlant(vec(13, 102), 6, calculateAngle(-30), 35);
+  drawFakePlant(vec(18, 107), 10, calculateAngle(-30), 127);
+  drawFakePlant(vec(23, 107), 11, calculateAngle(-30), 85);
+
+  drawFakePlant(vec(68, 106), 8, calculateAngle(-79), 73);
+  drawFakePlant(vec(75, 102), 5, calculateAngle(30), 60);
+  drawFakePlant(vec(83, 100), 8, calculateAngle(-30), 85);
+  drawFakePlant(vec(90, 102), 6, calculateAngle(-30), 35);
+  drawFakePlant(vec(95, 107), 10, calculateAngle(-30), 127);
+}
+
+function drawFakePlant(base, length, angle, offset) {
+  color("green");
+  const top = vec(base).addWithAngle(angle, length);
+  l = line(base, top, 3);
+  color("black");
+
+  //Alternate open and closed mouths
+  if ((ticks + offset) % (200 + offset * 3) < 75) {
+    char("a", top.add(0, -2));
+  } else {
+    char("b", top.add(0.5, -2));
   }
 }
 
@@ -159,7 +220,7 @@ function handleFlys() {
     fly.pos.x += 0.25 * fly.direction * difficulty;
 
     const f = char(
-      "d",
+      "c",
       vec(fly.pos.x, fly.pos.y + getFlyOffset(fly))
     ).isColliding;
 
